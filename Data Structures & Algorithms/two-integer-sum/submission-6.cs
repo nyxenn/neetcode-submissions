f938726db3834635c97f7787af4a5e0ee618536e
ʕ •ᴥ•ʔ <38733035+nyxenn@users.noreply.github.com>
2026-09-19 16:45:58 +0200

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        var diff_idx_lookup = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++) {
+            if (diff_idx_lookup.ContainsKey(nums[i])) {
+                return [ diff_idx_lookup[nums[i]], i ];
+            }
+
+            diff_idx_lookup.Add(target - nums[i], i);
+        }
+
+        return null;
+    }
+}
